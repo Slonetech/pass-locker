1e3dd8ae40bf7c7eb50512931a25a9713fa9cc59
hamida-mstafa <hamidamstafa@gmail.com>
2018-11-08 10:55:34 +0300

    displays the user account array

--- accounts.py
+++ accounts.py
@@ -6,3 +6,9 @@ class Accounts:
         self.password = password
 
     user_accounts = []
+
+    def save_account(self):
+        '''
+        this is a save function that appends the account to the user_accounts array
+        '''
+        Accounts.user_accounts.append(self)

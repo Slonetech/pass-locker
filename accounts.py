79b0ef4c40f5887f4a78445da038268cf812ccca
hamida-mstafa <hamidamstafa@gmail.com>
2018-11-08 10:51:20 +0300

    created a class that stores usernames

---
+++ accounts.py
@@ -0,0 +1,8 @@
+class Accounts:
+    def __init__(self, first_name, last_name, user_name, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+
+    user_accounts = []

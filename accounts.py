01a853ef320d8051f319020d69f116a939da40f0
hamida-mstafa <hamidamstafa@gmail.com>
2018-11-08 11:00:28 +0300

    calling a search function

--- accounts.py
+++ accounts.py
@@ -18,3 +18,11 @@ class Accounts:
         a function used to delete a selected account
         '''
         Accounts.user_accounts.remove(self)
+    @classmethod
+    def find_by_user_name(cls, user_name):
+        '''
+        this is a function tha checks whether the username provided by the username is available and if it is it returns the account
+        '''
+        for account in cls.user_accounts:
+            if account.user_name == user_name:
+                return account
